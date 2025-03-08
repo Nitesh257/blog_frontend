742211ef110eca9c23a3dd49695d36caa38745e8
Nitesh257 <mrb077899@gmail.com>
2025-03-08 12:13:53 +0530

    pinged backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react"; // Import useEffect
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
@@ -12,7 +13,20 @@ import Posts from "./pages/posts";
 import PostDetail from "./components/PostDetail";
 import PostForm from "./components/PostForm";
 import EditPost from "./components/EditPost";
+
+const keepBackendAlive = () => {
+  fetch("https://blogbackend-zz2d.onrender.com/")
+    .then(() => console.log("Pinged backend"))
+    .catch((err) => console.error("Ping failed", err));
+};
+
 const App = () => {
+  useEffect(() => {
+    keepBackendAlive(); // Ping on page load
+    const interval = setInterval(keepBackendAlive, 5 * 60 * 1000); // Ping every 5 minutes
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
   return (
     <>
       <BrowserRouter>
